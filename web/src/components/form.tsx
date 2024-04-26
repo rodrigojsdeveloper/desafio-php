c48ad9b3ac6ba5cfab937f98a35270ee3cae4bdf
Rodrigo Silva <rodrigojsdeveloper@gmail.com>
2024-04-26 18:33:09 -0300

    feat: add the frontend framework

---
+++ web/src/components/form.tsx
@@ -0,0 +1,9 @@
+export const Form = () => {
+  return (
+    <form>
+      <label>Nome</label>
+      <input placeholder="Digite o nome" />
+      <button type="submit">cadastrar</button>
+    </form>
+  )
+};
